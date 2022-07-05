20a03ad99cb07a42fbd1628207c6245e8670d4fe
pjgowtham <pjgowtham@gmail.com>
2022-07-05 19:43:26 +0530

    RMX2202: aux camera unlock fix

--- extract-files.sh
+++ extract-files.sh
@@ -59,10 +59,10 @@ function blob_fixup() {
             sed -i "/SystemCamera = / s/1;/0;/g" "${2}"
             ;;
         vendor/lib64/hw/com.qti.chi.override.so)
-            "${SIGSCAN}" -p "05 B8 05 94" -P "1F 20 03 D5" -f "${2}"
+            "${SIGSCAN}" -p "34 7D 05 94" -P "1F 20 03 D5" -f "${2}"
             ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
-            "${SIGSCAN}" -p "23 0B 00 94" -P "1F 20 03 D5" -f "${2}"
+            "${SIGSCAN}" -p "3F 0A 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
     esac
 }
